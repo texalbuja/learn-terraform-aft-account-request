40e06ad9f9479ff5a775619780c655cc3c9ffd61
Tex Albuja <talbuja@idtdna.com>
2021-12-23 14:28:11 -0500

    Create aft-dev account

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,30 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+module "development" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft-dev@infraws.xyz"
+    AccountName               = "aft-dev"
+    ManagedOrganizationalUnit = "Development"
+    SSOUserEmail              = "aft-dev@infraws.xyz"
+    SSOUserFirstName          = "Development"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Tex Albuja"
+    change_reason       = "Environment created for Development process"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "development"
+}
