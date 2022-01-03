6e5bf12b954a6af7ad73eb20208f0107630b3af5
Tex Albuja <talbuja@idtdna.com>
2022-01-03 10:58:24 -0500

    Adding CI Environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+module "ci" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft-ci@infraws.xyz"
+    AccountName               = "aft-ci"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "aft-ci@infraws.xyz"
+    SSOUserFirstName          = "CI"
+    SSOUserLastName           = "Sandbox"
+  }
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Update S3 Account by Global Customizations for CI"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "ci"
+}
 module "development" {
   source = "./modules/aft-account-request"
 
