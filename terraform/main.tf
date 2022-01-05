976367087983b300647fa89edb16adf8857b66e8
Tex Albuja <talbuja@idtdna.com>
2022-01-05 12:27:46 -0500

    Hello from AFT Demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -159,3 +159,31 @@ module "stage" {
 
   account_customizations_name = "stage"
 }
+
+module "demo" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aft-demo@infraws.xyz"
+    AccountName               = "aft-demo"
+    ManagedOrganizationalUnit = "Demo"
+    SSOUserEmail              = "aft-demo@infraws.xyz"
+    SSOUserFirstName          = "Demo"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Tex Albuja"
+    change_reason       = "Update S3 Account by Global Customizations for Demo"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "demo"
+}
